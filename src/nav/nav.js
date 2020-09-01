927ea892bda5ddfef853e9abe51d6b85a49d4465
superOldman <307485721@qq.com>
2020-09-01 17:27:39 +0800

    计算属性

--- src/nav/nav.js
+++ src/nav/nav.js
@@ -1,29 +1,33 @@
-import home from '../pages/home.vue';
-import vfor_test from '../pages/vfor_test.vue';
-import busSendTest from '../pages/busSendTest.vue';
-import busGetTest from '../pages/busGetTest.vue';
+// import home from '../pages/home.vue';
+// import vfor_test from '../pages/vfor_test.vue';
+// import busSendTest from '../pages/busSendTest.vue';
+// import busGetTest from '../pages/busGetTest.vue';
 
 const nav = [
     {
         path: '/home',
         name: 'home',
-        component: home
+        // component: home
     },
     {
         path: '/vfor_test',
         name: 'vfor_test',
-        component: vfor_test
+        // component: vfor_test
     },
     {
         path: '/busSendTest',
         name: 'busSendTest',
-        component: busSendTest
+        // component: busSendTest
     },
     {
         path: '/busGetTest',
         name: 'busGetTest',
-        component: busGetTest
+        // component: busGetTest
     },
+    {
+        path: '/computeTest',
+        name: 'computeTest',
+    }
 ]
 
 
---
+++ src/pages/computeTest.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <input type="text" v-model="a" />
+    <input type="text" v-model="b" />
+    <input type="text" v-model="c" />
+    <input type="text" v-model="d" />
+    <input type="text" v-model="e" />
+    <br />
+    <input type="text" v-model="text.name" />
+
+    <div>{{all}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      a: 0,
+      b: 0,
+      c: 0,
+      d: 0,
+      e: 0,
+      text: {},
+      arr: [
+        { name: "小明", age: 13 },
+        { name: "小红", age: 14 }
+      ]
+    };
+  },
+  computed: {
+    all: function() {
+      return (
+        Number(this.a) +
+        Number(this.b) +
+        Number(this.c) +
+        Number(this.d) +
+        Number(this.e)
+      );
+    }
+  },
+  created() {
+    // this.$set(this.text, "name", "12312");
+    this.text = {
+      name : '123131'
+    }
+  },
+  watch: {
+    text: {
+      deep: true,
+      handler(val) {
+        console.log("监听", val);
+      }
+    }
+  },
+  mounted() {
+    this.fn();
+  },
+  methods: {
+    fn() {
+      var a = this.arr.map(a => a.name);
+      console.log(a);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
